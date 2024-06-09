bb14d482df3fd9d5d31e6112aaf8ea8292c3dc19
Sheikh Saqib <sheikhsaqib40@gmail.com>
2024-06-09 19:20:02 +0100

    Migrated to Java 21

--- API/src/main/java/com/example/clinical_research/ClinicalResearchApplication.java
+++ API/src/main/java/com/example/clinical_research/ClinicalResearchApplication.java
@@ -2,9 +2,6 @@ package com.example.clinical_research;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClinicalResearchApplication {
@@ -13,14 +10,4 @@ public class ClinicalResearchApplication {
 		SpringApplication.run(ClinicalResearchApplication.class, args);
 	}
 
-	// @Bean
-	// public ConfigurableServletWebServerFactory servletContainer() {
-	// TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-	// factory.addConnectorCustomizers(connector -> {
-	// connector.setPort(8080);
-	// connector.setSecure(true);
-	// connector.setScheme("https");
-	// });
-	// return factory;
-	// }
 }
